9c41b1a003ef6734a941f1e20ac74f25e555ef75
Zhao Ji <693941639@qq.com>
2022-04-06 20:50:51 +0800

    debug

--- LengthOfLongestSubstring.java
+++ LengthOfLongestSubstring.java
@@ -6,12 +6,12 @@ public class LengthOfLongestSubstring {
         HashMap<Character, Integer> map = new HashMap<Character, Integer>();
         int max = 0;
         int left = 0;
-        for(int i = 0; i < s.length(); i ++){
-            if(map.containsKey(s.charAt(i))){
-                left = Math.max(left,map.get(s.charAt(i)) + 1);
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                left = Math.max(left, map.get(s.charAt(i)) + 1);
             }
-            map.put(s.charAt(i),i);
-            max = Math.max(max,i-left+1);
+            map.put(s.charAt(i), i);
+            max = Math.max(max, i - left + 1);
         }
         return max;
     }
