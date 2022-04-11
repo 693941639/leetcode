793de6f32f34865807e977322a2aaf98f30af7cd
Johnson <johnson.zhao@planetart.cn>
2022-04-11 17:38:23 +0800

    debug

--- Test.java
+++ Test.java
@@ -6,60 +6,47 @@ public class Test {
     public static void main(String[] args) {
         Test t = new Test();
 
-        int[] a = {};
-        int[] b = {};
-
-        System.out.println(t.findMedianSortedArrays(a, b));
+        System.out.println(t.longestPalindrome("abbbbbbbbbbbbbba"));
     }
 
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int middle = 0;
-        int curr = 0;
-        int posOne = 0;
-        int posTwo = 0;
-        ArrayList<Integer> rel = new ArrayList<>();
+    public String longestPalindrome(String s) {
+        int max = 0;
+        boolean isEqual = true;
+        StringBuffer rel = new StringBuffer();
 
-        int length = nums1.length + nums2.length;
+        int type = (s.length() & 1);
 
-        middle = length / 2 + 1;
+        type = (type == 1) ? 0 : 1;
 
-        while (curr < middle) {
-            if (posOne == nums1.length) {
-                rel.add(curr, nums2[posTwo]);
-                posTwo++;
-                curr++;
-                continue;
+        for (int i = 0; i < s.length() - 1; i++) {
+            int start = i;
+            int last = s.lastIndexOf(s.substring(i, i + 1));
+            int end = last;
+
+            if (end > start) {
+                while (end - start != type) {
+                    if (!this.checkSame(s, start++, end--)) {
+                        isEqual = false;
+                        break;
+                    }
+                }
+
+                if (isEqual && (last - i + 1) > max) {
+                    max = last - i + 1;
+                    rel.setLength(0);
+                    rel.append(s.substring(i, max));
+                }
             }
-
-            if (posTwo == nums2.length) {
-                rel.add(curr, nums1[posOne]);
-                posOne++;
-                curr++;
-                continue;
-            }
-
-            if (nums1[posOne] > nums2[posTwo]) {
-                rel.add(curr, nums2[posTwo]);
-                posTwo++;
-            } else if (nums1[posOne] < nums2[posTwo]) {
-                rel.add(curr, nums1[posOne]);
-                posOne++;
-            } else {
-                rel.add(curr, nums1[posOne]);
-                curr++;
-                rel.add(curr, nums2[posTwo]);
-
-                posOne++;
-                posTwo++;
-            }
-
-            curr++;
         }
 
-        if ((length & 1) == 1) {
-            return (double) rel.get(middle - 1);
-        } else {
-            return (rel.get(middle - 1) + rel.get(middle - 2)) / 2.0;
+        return rel.toString();
+    }
+
+    public boolean checkSame(String s, int firstIndex, int secondIndex) {
+        if (s.substring(firstIndex, firstIndex + 1).equals(s.substring(secondIndex, secondIndex + 1))) {
+            return true;
         }
+
+        return false;
     }
 }
